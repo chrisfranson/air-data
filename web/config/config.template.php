d3f63031ddee36c4b3585cf3180519c818293938
Chris Franson <chris.franson@gmail.com>
2016-02-21 21:19:47 -0600

    Prepend data published from the Photon to a table
    - Include jQuery for super-easy javascripting
    - Add a listener to the Photon’s “Particles” event
    - Parse JSON data
    - Prepend a row to the top of the table for each event

---
+++ web/config/config.template.php
@@ -0,0 +1,5 @@
+<?php
+
+$particle_deviceID = '<<hex device id here>>';
+$particle_accessToken = '<<hex access token here>>';
+
--- web/public/index.php
+++ web/public/index.php
@@ -1,36 +1,67 @@
-<!DOCTYPE HTML>
+<!doctype html>
 <html>
+<head>
+    <title>DustDuino Data!</title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css">
+</head>
 <body>
-    <span id="uptime"></span><br>
-    <span id="tstamp"></span>
+<?php
 
-    <br><br>
-    <button onclick="start()">Connect</button>
+    require_once('../config/config.php');
 
+?>
+    <table class="data-table table">
+        <thead>
+            <tr>
+                <th>Timestamp</th>
+                <th>PM10Conc</th>
+                <th>PM25Conc</th>
+                <th>PM10Count</th>
+                <th>PM25Count</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+
+
+
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
     <script type="text/javascript">
-    function start() {
 
-        document.getElementById("uptime").innerHTML = "Waiting for data...";
-        var deviceID = "<<hex device id here>>";
-        var accessToken = "<<hex access token here>>";
-        var eventSource = new EventSource("https://api.spark.io/v1/devices/" + deviceID + "/events/?access_token=" + accessToken);
+    $(document).ready(function() {
+
+        var deviceID = "<?php echo $particle_deviceID; ?>";
+        var accessToken = "<?php echo $particle_accessToken; ?>";
+        var eventSource = new EventSource("https://api.particle.io/v1/devices/" + deviceID + "/events/?access_token=" + accessToken);
 
         eventSource.addEventListener('open', function(e) {
-            console.log("Opened!"); },false);
+            console.log("Opened!");
+        },false);
 
         eventSource.addEventListener('error', function(e) {
-            console.log("Errored!"); },false);
+            console.log("Errored!");
+        },false);
 
-        eventSource.addEventListener('Uptime', function(e) {
-            var parsedData = JSON.parse(e.data);
-            var tempSpan = document.getElementById("uptime");
-            var tsSpan   = document.getElementById("tstamp");
-            tempSpan.innerHTML = "Core: " + parsedData.coreid + " uptime: " + parsedData.data + " (h:m:s)";
-            tempSpan.style.fontSize = "28px";
-            tsSpan.innerHTML = "At timestamp " + parsedData.published_at;
-            tsSpan.style.fontSize = "9px";
+        eventSource.addEventListener('Particles', function(e) {
+            var rawData = JSON.parse(e.data),
+                data = JSON.parse(rawData.data);
+
+            var $dataRow = $(
+                '<tr>' +
+                    '<td>' + rawData.published_at + '</td>' +
+                    '<td>' + data.PM10Conc + '</td>' +
+                    '<td>' + data.PM25Conc + '</td>' +
+                    '<td>' + data.PM10count + '</td>' +
+                    '<td>' + data.PM25count + '</td>' +
+                '</tr>'
+            );
+
+            $('.data-table tbody').prepend($dataRow);
+
         }, false);
-    }
+    });
+
     </script>
 </body>
 </html>
